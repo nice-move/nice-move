745784301cf7f868f64ee96acc9f51bfccd6cfc2
Eric Chen <airkro@qq.com>
2018-01-17 16:34:20 +0800

    1.0.0

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  extends: ["./stylelint-order.json", "stylelint-config-standard"],
+  plugins: ["stylelint-order", "stylelint-scss"],
+  rules: {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+    "at-rule-empty-line-before": [
+      "always",
+      {
+        ignoreAtRules: ["else", "import"]
+      }
+    ],
+    "block-closing-brace-newline-after": [
+      "always",
+      {
+        ignoreAtRules: ["if"]
+      }
+    ],
+    "declaration-colon-newline-after": null,
+    "property-no-vendor-prefix": true,
+    "scss/declaration-nested-properties": "never",
+    "selector-pseudo-element-colon-notation": "double",
+    "value-keyword-case": "lower",
+    "value-no-vendor-prefix": true
+  }
+};
--- readme.md
+++ readme.md
@@ -1,3 +1,31 @@
 # stylelint-config-airkro
 
 stylelint config for personal use
+
+## Installation
+
+```js
+npm install stylelint-config-airkro -O
+
+// or
+
+yarn stylelint-config-airkro -O
+```
+
+## Usage
+
+Add following settings to your `.stylelintrc`:
+
+```json
+{
+  "extends": [
+    "stylelint-config-airkro"
+  ]
+}
+```
+
+## Related
+
+* [stylelint-order](https://www.npmjs.com/package/stylelint-order)
+* [stylelint-scss](https://www.npmjs.com/package/stylelint-scss)
+* [stylelint-config-standard](https://www.npmjs.com/package/stylelint-config-standard)

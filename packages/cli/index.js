46dcbde061332a7a8d43dd289005f1aad9faa2e8
Eric Chen <airkro@qq.com>
2019-09-05 22:17:34 +0800

    fix

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,32 +1,22 @@
 #!/usr/bin/env node
 
-'use strict';
-
-if (process.stdout.isTTY) {
-  // istanbul ignore next
-  process.env.FORCE_COLOR = '1';
-}
-
-// Do not terminate main process on SIGINT
 process.on('SIGINT', () => {});
 
-const cmdLine = require('commander');
-
+const { resolve } = require('path');
+const program = require('commander');
 const lintStaged = require('lint-staged');
 
-cmdLine
-  .option('-c, --config [path]', 'Path to configuration file')
-  .option('-r, --relative', 'Pass relative file path to tasks')
+program
   .option('-x, --shell', 'Skip parsing of tasks for better shell support')
   .option('-q, --quiet', 'Disable lint-staged’ s own console output')
   .parse(process.argv);
 
 lintStaged({
-  configPath: cmdLine.config,
-  relative: !!cmdLine.relative,
-  shell: !!cmdLine.shell,
-  quiet: !!cmdLine.quiet,
-  debug: !!cmdLine.debug
+  configPath: resolve(__dirname, 'config.json'),
+  relative: process.cwd(),
+  shell: !!program.shell,
+  quiet: !!program.quiet,
+  debug: false
 })
   .then(passed => {
     process.exitCode = passed ? 0 : 1;

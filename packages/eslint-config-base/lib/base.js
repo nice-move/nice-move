304aafc96edb0c7805bc9c1240f0c73d911d0d9e
Eric Chen <airkro@qq.com>
2020-10-16 14:35:21 +0800

    feat(eslint-config): add globals

--- packages/eslint-config-base/lib/base.js
+++ packages/eslint-config-base/lib/base.js
@@ -16,22 +16,30 @@ module.exports = {
     [env]: true,
     node: true,
   },
+  globals: {
+    process: 'readonly',
+    __dirname: 'readonly',
+    __filename: 'readonly',
+  },
   rules: {
     camelcase: 'off',
     'no-console': 'off',
     'no-nested-ternary': 'off',
     'promise/always-return': 'off',
     'promise/avoid-new': 'off',
-    'promise/catch-or-return': [
-      'warn',
-      {
-        allowFinally: true,
-      },
-    ],
+    'promise/catch-or-return': ['warn', { allowFinally: true }],
     'promise/no-callback-in-promise': 'off',
     'unicorn/expiring-todo-comments': 'off',
     'unicorn/no-null': 'off',
     'unicorn/prefer-set-has': 'off',
     'unicorn/prevent-abbreviations': 'off',
   },
+  overrides: [
+    {
+      files: 'src/**',
+      globals: {
+        __webpack_public_path__: 'readonly',
+      },
+    },
+  ],
 };

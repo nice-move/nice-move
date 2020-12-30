ca991cdfcbc4e394e56a026aeea69e6c964e9a05
Eric Chen <airkro@qq.com>
2020-12-30 11:11:36 +0800

    feat(eslint-config): move some rules to `garou`

--- packages/eslint-config-base/lib/base.cjs
+++ packages/eslint-config-base/lib/base.cjs
@@ -54,6 +54,7 @@ module.exports = {
       'error',
       { allowImplicit: true, checkForEach: true },
     ],
+    'consistent-return': 'warn',
     'eslint-comments/no-unused-disable': 'warn',
     'eslint-comments/no-unused-enable': 'warn',
     'global-require': 'off',
--- packages/eslint-config-react/lib/react.cjs
+++ packages/eslint-config-react/lib/react.cjs
@@ -9,7 +9,11 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'react/destructuring-assignment': 'off',
-    'react/jsx-boolean-value': ['warn', 'always'],
+    'react/jsx-boolean-value': 'off',
+    'react/jsx-curly-brace-presence': [
+      'error',
+      { props: 'ignore', children: 'never' },
+    ],
     'react/jsx-no-useless-fragment': 'warn',
     'react/jsx-props-no-spreading': 'off',
     'react/jsx-uses-react': 'off',

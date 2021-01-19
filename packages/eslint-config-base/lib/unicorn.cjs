0c11970308996188c9b536bdb4bb2d1897eba9de
Eric Chen <airkro@qq.com>
2021-01-19 18:05:49 +0800

    feat(eslint-config): new rules

--- packages/eslint-config-base/lib/unicorn.cjs
+++ packages/eslint-config-base/lib/unicorn.cjs
@@ -2,19 +2,11 @@ module.exports = {
   extends: ['plugin:unicorn/recommended', 'prettier/unicorn'],
   rules: {
     'unicorn/expiring-todo-comments': 'off',
-    'unicorn/import-index': 'off',
+    'unicorn/import-index': ['warn', { ignoreImports: true }],
     'unicorn/import-style': 'off',
+    'unicorn/no-array-reduce': 'off',
     'unicorn/no-null': 'off',
-    'unicorn/no-reduce': 'off',
     'unicorn/prefer-set-has': 'off',
     'unicorn/prevent-abbreviations': 'off',
   },
-  overrides: [
-    {
-      files: '*.cjs',
-      rules: {
-        'unicorn/import-index': 'warn',
-      },
-    },
-  ],
 };

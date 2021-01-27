ca62c5d842bb160cbcbff55eb4d85ccdbb1dbca5
Eric Chen <airkro@qq.com>
2021-01-27 09:09:05 +0800

    feat(eslint-config): better `cosmiconfig` support

--- packages/eslint-config-base/lib/unicorn.cjs
+++ packages/eslint-config-base/lib/unicorn.cjs
@@ -1,9 +1,11 @@
 module.exports = {
   extends: ['plugin:unicorn/recommended', 'prettier/unicorn'],
   rules: {
+    'unicorn/consistent-destructuring': 'warn',
     'unicorn/expiring-todo-comments': 'off',
     'unicorn/import-index': ['warn', { ignoreImports: true }],
     'unicorn/import-style': 'off',
+    'unicorn/no-array-for-each': 'off',
     'unicorn/no-array-reduce': 'off',
     'unicorn/no-null': 'off',
     'unicorn/prefer-set-has': 'off',

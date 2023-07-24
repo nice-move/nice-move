2279c673b0b4ada95abdfbb055c4f6ed9fa2c7c6
Eric <airkro@qq.com>
2023-07-24 15:15:46 +0800

    chore(deps): update

--- packages/eslint-config-base/lib/typescript.cjs
+++ packages/eslint-config-base/lib/typescript.cjs
@@ -1,24 +1,11 @@
 'use strict';
 
-const {
-  overrides: [{ rules: base }],
-} = require('@typescript-eslint/eslint-plugin/dist/configs/eslint-recommended.js');
-const {
-  rules,
-} = require('@typescript-eslint/eslint-plugin/dist/configs/recommended.js');
-
-function filterObject(object, filter) {
-  return Object.fromEntries(
-    Object.entries(object).filter(([key, value]) => filter(key, value)),
-  );
-}
-
 module.exports = {
   overrides: [
     {
       files: ['*.ts', '*.tsx', '*.mts', '*.cts'],
       parser: '@typescript-eslint/parser',
-      plugins: ['@typescript-eslint'],
+      plugins: ['@typescript-eslint/eslint-plugin'],
       // parserOptions: {
       //   tsconfigRootDir: process.cwd(),
       //   project: ['./tsconfig.json'],
@@ -28,9 +15,33 @@ module.exports = {
       //   'airbnb-typescript',
       // ],
       rules: {
-        ...filterObject(base, (_key, value) => value !== 'off' && value !== 0),
-        ...rules,
+        '@typescript-eslint/ban-types': 'error',
+        '@typescript-eslint/no-array-constructor': 'error',
+        '@typescript-eslint/no-duplicate-enum-values': 'error',
+        '@typescript-eslint/no-dynamic-delete': 'error',
+        '@typescript-eslint/no-extra-non-null-assertion': 'error',
+        '@typescript-eslint/no-extraneous-class': 'error',
+        '@typescript-eslint/no-invalid-void-type': 'error',
+        '@typescript-eslint/no-loss-of-precision': 'error',
+        '@typescript-eslint/no-misused-new': 'error',
+        '@typescript-eslint/no-namespace': 'error',
+        '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'error',
+        '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
+        '@typescript-eslint/no-non-null-assertion': 'error',
+        '@typescript-eslint/no-this-alias': 'error',
+        '@typescript-eslint/no-unnecessary-type-constraint': 'error',
+        '@typescript-eslint/no-unsafe-declaration-merging': 'error',
+        '@typescript-eslint/no-useless-constructor': 'error',
+        '@typescript-eslint/no-var-requires': 'error',
+        '@typescript-eslint/prefer-as-const': 'error',
+        '@typescript-eslint/prefer-literal-enum-member': 'error',
+        '@typescript-eslint/prefer-ts-expect-error': 'error',
+        '@typescript-eslint/triple-slash-reference': 'error',
+        '@typescript-eslint/unified-signatures': 'error',
+
+        '@typescript-eslint/no-explicit-any': 'warn',
         '@typescript-eslint/ban-ts-comment': 0,
+        '@typescript-eslint/no-empty-function': 0,
         '@typescript-eslint/no-unused-vars': [
           'warn',
           {

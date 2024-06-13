668b73b2806ffaef922a43fbc43e350c96b4034b
Eric <airkro@qq.com>
2024-06-13 12:42:47 +0800

    feat(syncpack-config): read pnpm workspaces

---
+++ packages/syncpack-config/define.cjs
@@ -0,0 +1,95 @@
+'use strict';
+
+const { fileURLToPath } = require('node:url');
+const { join } = require('node:path');
+const { findWorkspaces } = require('find-workspaces');
+
+module.exports = function defineConfig(url, config = {}) {
+  let pkg = {};
+
+  try {
+    pkg = require(
+      url === '~'
+        ? join(process.cwd(), 'package.json')
+        : join(fileURLToPath(url), '../package.json'),
+    );
+  } catch (error) {
+    console.error(error);
+  }
+
+  return {
+    ...config,
+    lintFormatting: false,
+    customTypes: {
+      ...config.customTypes,
+      engines: {
+        path: 'engines',
+        strategy: 'versionsByName',
+      },
+      packageManager: {
+        path: 'packageManager',
+        strategy: 'name@version',
+      },
+    },
+    semverGroups: [
+      ...(config.semverGroups || []),
+      {
+        dependencies: [
+          'typescript',
+          'react',
+          'react-dom',
+          '@types/react-dom',
+          '@types/react',
+        ],
+        range: '~',
+      },
+      {
+        dependencyTypes: ['local', 'packageManager'],
+        range: '',
+      },
+      {
+        dependencyTypes: ['!engines'],
+        range: '^',
+      },
+    ],
+    versionGroups: [
+      {
+        dependencies: ['@types/**'],
+        dependencyTypes: ['!peer'],
+        isBanned: true,
+        label: '@types packages should only be under peerDependencies',
+      },
+      {
+        dependencies: [
+          'react',
+          'react-dom',
+          '@types/react-dom',
+          '@types/react',
+        ],
+        dependencyTypes: ['!local'],
+        label: 'Pin react',
+        policy: 'sameRange',
+      },
+      pkg.packageManager?.startsWith?.('pnpm')
+        ? {
+            dependencies: findWorkspaces().map((item) => item.package.name),
+            dependencyTypes: ['!local'],
+            label: 'Pin pnpm workspace',
+            pinVersion: 'workspace:~',
+          }
+        : undefined,
+      ...(config.versionGroups || []),
+      {
+        dependencies: ['node'],
+        dependencyTypes: ['engines'],
+        label: 'Pin engines.node',
+        pinVersion: pkg.engines?.node || '^20.0.0',
+      },
+      {
+        dependencyTypes: ['!local'],
+        label: 'Pin others',
+        preferVersion: 'highestSemver',
+      },
+    ].filter(Boolean),
+  };
+};
--- syncpack.config.mjs
+++ syncpack.config.mjs
@@ -1,34 +1,20 @@
 // @ts-check
 
-import defineConfig from '@nice-move/syncpack-config';
+import defineConfig from '@nice-move/syncpack-config/define.cjs';
 
 export default defineConfig(import.meta.url, {
   versionGroups: [
     {
-      dependencies: [
-        '@nice-move/eslint-plugin-html',
-        '@nice-move/prettier-plugin-package-json',
-      ],
-      dependencyTypes: ['!local'],
-      preferVersion: 'highestSemver',
-    },
-    {
       dependencies: ['@nice-move/eslint-config-*'],
       dependencyTypes: ['local'],
-      label: 'Same version',
-      pinVersion: '0.11.8',
+      label: 'Same version 1',
+      pinVersion: '0.11.9',
     },
     {
       dependencies: ['@nice-move/all-in-*'],
       dependencyTypes: ['local'],
       label: 'Same version 2',
-      pinVersion: '0.1.0',
-    },
-    {
-      dependencies: ['@nice-move/*'],
-      dependencyTypes: ['!local'],
-      label: 'Pin pnpm workspace',
-      pinVersion: 'workspace:~',
+      pinVersion: '0.1.1',
     },
   ],
 });

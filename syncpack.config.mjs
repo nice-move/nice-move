aa4176afd0716bc053ae3a71c4ab4327596e0c02
Eric <airkro@qq.com>
2024-07-31 18:17:55 +0800

    chore: publish

--- syncpack.config.mjs
+++ syncpack.config.mjs
@@ -21,7 +21,7 @@ export default defineConfig(import.meta.url, {
       dependencies: ['@nice-move/all-in-*'],
       dependencyTypes: ['local'],
       label: 'Same version 2',
-      pinVersion: '0.1.4',
+      pinVersion: '0.1.5',
     },
   ],
 });

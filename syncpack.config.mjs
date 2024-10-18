0b7aa01c624ac00af0f3718a0ec07d5fcd122064
Eric <airkro@qq.com>
2024-10-18 10:16:05 +0800

    chore(pnpm): use catalog

--- syncpack.config.mjs
+++ syncpack.config.mjs
@@ -4,21 +4,16 @@ import defineConfig from '@nice-move/syncpack-config/define.cjs';
 export default defineConfig(import.meta.url, {
   versionGroups: [
     {
-      dependencies: ['typescript'],
-      packages: ['@nice-move/tsconfig'],
-      pinVersion: '~5.5.4',
-    },
-    {
       dependencies: ['@nice-move/eslint-config-*'],
       dependencyTypes: ['local'],
       label: 'Same version 1',
-      pinVersion: '0.11.24',
+      pinVersion: '0.11.25',
     },
     {
       dependencies: ['@nice-move/all-in-*'],
       dependencyTypes: ['local'],
       label: 'Same version 2',
-      pinVersion: '0.2.4',
+      pinVersion: '0.2.5',
     },
   ],
 });

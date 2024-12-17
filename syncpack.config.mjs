28026b84d56c761e1cad4b9ef37320690e9d6cd3
Eric <airkro@qq.com>
2024-12-17 14:34:45 +0800

    chore(deps): update

--- syncpack.config.mjs
+++ syncpack.config.mjs
@@ -7,13 +7,13 @@ export default defineConfig(import.meta.url, {
       dependencies: ['@nice-move/eslint-config-*'],
       dependencyTypes: ['local'],
       label: 'Same version 1',
-      pinVersion: '0.11.31',
+      pinVersion: '0.11.32',
     },
     {
       dependencies: ['@nice-move/all-in-*'],
       dependencyTypes: ['local'],
       label: 'Same version 2',
-      pinVersion: '0.2.14',
+      pinVersion: '0.2.15',
     },
   ],
 });

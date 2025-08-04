02e57f9e6143615049c74329814907475d52332a
Eric <airkro@qq.com>
2025-08-04 10:40:36 +0800

    fix: bug

--- syncpack.config.mjs
+++ syncpack.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig(import.meta.url, {
       dependencies: ['@nice-move/all-in-*'],
       dependencyTypes: ['local'],
       label: 'Same version 2',
-      pinVersion: '0.6.4',
+      pinVersion: '0.6.5',
     },
   ],
 });

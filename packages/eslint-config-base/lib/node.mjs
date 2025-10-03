145dabbebcb8804ced5113d4382f552c39f2c975
Eric Chen <airkro@qq.com>
2025-10-04 01:59:36 +0800

    feat(eslint-config): node-builtins allowExperimental

--- packages/eslint-config-base/lib/node.mjs
+++ packages/eslint-config-base/lib/node.mjs
@@ -44,7 +44,12 @@ export default [
       'n/no-process-exit': 'warn',
       'n/no-unsupported-features/es-builtins': 'error',
       'n/no-unsupported-features/es-syntax': 'error',
-      'n/no-unsupported-features/node-builtins': 'error',
+      'n/no-unsupported-features/node-builtins': [
+        'error',
+        {
+          allowExperimental: true,
+        },
+      ],
       'n/prefer-global/buffer': 'warn',
       'n/prefer-global/console': 'warn',
       'n/prefer-global/process': 'warn',
--- syncpack.config.mjs
+++ syncpack.config.mjs
@@ -7,13 +7,13 @@ export default defineConfig(import.meta.url, {
       dependencies: ['@nice-move/eslint-config-*'],
       dependencyTypes: ['local'],
       label: 'Same version 1',
-      pinVersion: '0.13.7',
+      pinVersion: '0.13.8',
     },
     {
       dependencies: ['@nice-move/all-in-*'],
       dependencyTypes: ['local'],
       label: 'Same version 2',
-      pinVersion: '0.6.16',
+      pinVersion: '0.6.17',
     },
   ],
 });
